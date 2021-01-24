35faf8a532a9954f1deedffb6b5db0859b70a0a0
Tomas <tomas.bognar@outlook.com>
2021-01-24 21:16:04 +0100

    - added some ractive repository tests

--- src/main/java/guru/springframework/repositories/reactive/UnitOfMeasureReactiveRepository.java
+++ src/main/java/guru/springframework/repositories/reactive/UnitOfMeasureReactiveRepository.java
@@ -1,4 +1,10 @@
 package guru.springframework.repositories.reactive;
 
-public interface UnitOfMeasureReactiveRepository {
+import guru.springframework.domain.UnitOfMeasure;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
+
+public interface UnitOfMeasureReactiveRepository extends ReactiveMongoRepository<UnitOfMeasure, String> {
+
+    Mono<UnitOfMeasure> findByDescription(String description);
 }
